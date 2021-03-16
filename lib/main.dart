edea82faa27a0c6a71cd8d7cedff53e474f7a1f4
Reuben Turner <groovinchip@gmail.com>
2021-03-16 12:43:19 -0400

    Add bitsdojo

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
+  doWhenWindowReady(() {
+    final initialSize = Size(600, 450);
+    appWindow.minSize = initialSize;
+    appWindow.size = initialSize;
+    appWindow.alignment = Alignment.center;
+    appWindow.show();
+  });
 }
 
 class MyApp extends StatelessWidget {
