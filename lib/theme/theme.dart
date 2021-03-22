78296cc1a89705321fce96b8e150fb94f8e50984
Reuben Turner <groovinchip@gmail.com>
2021-03-21 22:58:05 -0400

    Extract theme out to a class

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,5 +1,57 @@
 import 'package:flutter/material.dart';
 
+class Themes {
+  static ThemeData get cookieTheme {
+    return ThemeData(
+      brightness: Brightness.dark,
+      primarySwatch: Colors.brown,
+      accentColor: Colors.brown.shade300,
+      canvasColor: Color.lerp(Colors.brown.shade800, Colors.black, .5),
+      textTheme: TextTheme(
+        headline5: TextStyle(
+          color: Colors.white,
+        ),
+        subtitle1: TextStyle(
+          color: Colors.white,
+        ),
+        subtitle2: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+      inputDecorationTheme: InputDecorationTheme(
+        filled: true,
+        fillColor: Colors.brown.shade800,
+        labelStyle: TextStyle(
+          color: Colors.white,
+        ),
+        enabledBorder: UnderlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.brown.shade200,
+          ),
+        ),
+      ),
+      textSelectionTheme: TextSelectionThemeData(
+        cursorColor: Colors.white,
+        selectionColor: Colors.brown.shade200,
+      ),
+      iconTheme: IconThemeData(
+        color: Colors.white,
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          //shape: BeveledRectangleBorder(),
+          //padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16)
+          visualDensity: VisualDensity.comfortable,
+        ),
+      ),
+      checkboxTheme: CheckboxThemeData(
+        fillColor: MaterialBrown(),
+      ),
+      visualDensity: VisualDensity.adaptivePlatformDensity,
+    );
+  }
+}
+
 /// A MaterialStateColor used for this app's CheckboxThemeData fillColor
 class MaterialBrown extends MaterialStateColor {
   const MaterialBrown() : super(_defaultColor);
