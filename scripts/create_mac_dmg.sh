2222b1353b90c5584512c15db87333cdc4cd0271
Reuben Turner <groovinchip@gmail.com>
2021-03-24 19:12:11 -0400

    Fix script

--- scripts/create_mac_dmg.sh
+++ scripts/create_mac_dmg.sh
@@ -2,7 +2,7 @@
 test -f groovin_create.dmg && rm groovin_create.dmg
 create-dmg \
   --volname "Groovin Create Installer" \
-  --volicon "./assets/groovin_create_installer.icns" \
+  --volicon "../assets/groovin_create_installer.icns" \
   --window-pos 200 120 \
   --window-size 800 530 \
   --icon-size 130 \
@@ -11,5 +11,5 @@ create-dmg \
   --hide-extension "groovin_create.app" \
   --app-drop-link 540 250 \
   --hdiutil-quiet \
-  "build/macos/Build/Products/Release/groovin_create.dmg" \
-  "build/macos/Build/Products/Release/groovin_create.app"
+  "../build/macos/Build/Products/Release/groovin_create.dmg" \
+  "../build/macos/Build/Products/Release/groovin_create.app"
